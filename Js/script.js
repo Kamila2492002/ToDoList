d63615886fb59cbcc30cadf4451cde913485397f
Kamila Król <kamilakrol01@gmail.com>
2022-11-11 15:35:44 +0100

    remove tasks

--- Js/script.js
+++ Js/script.js
@@ -1,7 +1,18 @@
 {
  let tasks = [];
 
- 
+ const addNewTask = (newTaskContent) => {
+    tasks.push({
+        content: newTaskContent,
+    });
+    
+    render();
+};
+
+const removeTask = (taskIndex) => {
+    tasks.splice(taskIndex, 1);
+        render();
+}
 
  const render = () => {
     let htmlString = "";
@@ -9,22 +20,24 @@
     for(const task of tasks) {
         htmlString += ` 
         <li 
-            ${task.done ? "style=\"text-decoration: line-through\"" : ""}>
+            ${task.done ? "style=\"text-decoration: line-through\"" : ""}
+        >
+            <button class="js-remove">Usuń</button>
             ${task.content}
         </li>
         `;
     }
 
-    document.querySelector(".js-tasks").inneHTML = htmlString;
- };
-
-const addNewTask = (newTaskContent) => {
-    tasks.push({
-        content: newTaskContent,
-    });
+    document.querySelector(".js-tasks").innerHTML = htmlString;
     
-    render();
-};
+    const removeButtons = document.querySelectorAll(".js-remove");
+
+    removeButtons.forEach((removeButton, index) => {
+        removeButton.addEventListener("click", () => {
+            removeTask(index);
+        });
+    });
+ };
 
 const onFormSubmit = (event) => {
     event.preventDefault();
